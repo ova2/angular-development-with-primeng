6264c57449bab9f39e14a9f295a079ab5d47e3b7
Oleg Varaksin <ovaraksin@googlemail.com>
2017-03-27 22:19:54 +0200

    Fixed hour format issue

--- chapter3/calendar/src/app/section/calendar.component.ts
+++ chapter3/calendar/src/app/section/calendar.component.ts
@@ -7,44 +7,31 @@ import {SelectItem} from 'primeng/components/common/api';
     templateUrl: 'calendar.component.html'
 })
 export class CalendarComponent {
-
     basicDateInput: Date;
-
     localizedDateInput: Date;
-
     advancedDateInput: Date;
-
     timeDateInput: Date;
-
     inlineDateInput: Date;
-
     minDate: Date;
-
     maxDate: Date;
-
     time: boolean;
-
     de: any;
-
     types: SelectItem[];
-
-    selectedType: string ='12';
-
+    selectedHourFormat: string = '12';
     msgs: Message[] = [];
-
-    constructor() {
-
-    }
 
     ngOnInit() {
         this.de = {
             firstDayOfWeek: 1,
-            dayNames: [ 'Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag' ],
-            dayNamesShort: [ 'Son', 'Mon', 'Die', 'Mit', 'Don', 'Fre', 'Sam' ],
-            dayNamesMin: [ 'S', 'M', 'D', 'M ', 'D', 'F ', 'S' ],
-            monthNames: [ 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember' ],
-            monthNamesShort: [ 'Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez' ]
-        }
+            dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+            dayNamesShort: ['Son', 'Mon', 'Die', 'Mit', 'Don', 'Fre', 'Sam'],
+            dayNamesMin: ['S', 'M', 'D', 'M ', 'D', 'F ', 'S'],
+            monthNames: [
+                'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli',
+                'August', 'September', 'Oktober', 'November', 'Dezember'
+            ],
+            monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez']
+        };
 
         this.types = [];
         this.types.push({label: '12H Format', value: '12'});
@@ -53,7 +40,7 @@ export class CalendarComponent {
         let today = new Date();
         let month = today.getMonth();
         let year = today.getFullYear();
-        let prevMonth = (month === 0) ? 11 : month -1;
+        let prevMonth = (month === 0) ? 11 : month - 1;
         let prevYear = (prevMonth === 11) ? year - 1 : year;
         let nextMonth = (month === 11) ? 0 : month + 1;
         let nextYear = (nextMonth === 0) ? year + 1 : year;
@@ -65,15 +52,28 @@ export class CalendarComponent {
         this.maxDate.setFullYear(nextYear);
     }
 
-    onSelect(){
+    onSelect() {
         this.msgs.push({severity: 'info', summary: 'The calendar date is selected'});
     }
 
-    onBlur(){
+    onBlur() {
         this.msgs.push({severity: 'info', summary: 'Calendar lost the focus'});
     }
 
-    onFocus(){
+    onFocus() {
         this.msgs.push({severity: 'info', summary: 'Calendar got the focus'});
+    }
+
+    set hourFormat(hourFormat: string) {
+        this.selectedHourFormat = hourFormat;
+        if (this.timeDateInput) {
+            // update reference to the input value in order to trigger proper change detection.
+            // Angular doesn't trigger change detection if the object reference stays the same.
+            this.timeDateInput = new Date(this.timeDateInput.getTime());
+        }
+    }
+
+    get hourFormat(): string {
+        return this.selectedHourFormat;
     }
 }
