c9fa9be91a17d9a1e0116fa5b536d55bb993c574
Sudheer Jonna <sudheer.jonna@gmail.com>
2017-03-29 22:31:45 +0800

    Cosmotics for display

--- chapter3/checkbox/src/app/section/checkbox.component.ts
+++ chapter3/checkbox/src/app/section/checkbox.component.ts
@@ -7,12 +7,13 @@ import {Message} from 'primeng/components/common/api';
 })
 export class CheckboxComponent {
     msgs: Message[] = [];
-    
+
     checked: boolean;
     selectedVersions: string[]=['AngularJS1.0','AngularV4.0'];
-    status: any;
+    status: any= true;
     
-    onComplete() {
+    onChange() {
+        this.msgs=[]
         this.msgs.push({severity: 'info', summary: 'Checkbox status is changed'});
     }
 }
