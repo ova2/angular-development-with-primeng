c97390fad94777be105957cb4aae559b41722c9c
Oleg Varaksin <oleg.varaksin@sbb.ch>
2017-04-13 15:46:55 +0200

    Deleted steps

--- chapter7/steps/src/main.jit.ts
+++
@@ -1,8 +0,0 @@
-// Webpack creates a link to the main.css and put it into the index.html
-import './assets/css/main.scss';
-
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule}  from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
