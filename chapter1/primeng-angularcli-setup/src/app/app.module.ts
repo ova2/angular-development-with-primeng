a3e2815f47d3cfebc71076239f64d14303837c31
Oleg Varaksin <ovaraksin@googlemail.com>
2017-05-05 00:21:46 +0200

    Updated dependencies

--- chapter1/primeng-angularcli-setup/src/app/app.module.ts
+++ chapter1/primeng-angularcli-setup/src/app/app.module.ts
@@ -1,26 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 // import needed PrimeNG modules here
 import {MessagesModule} from 'primeng/components/messages/messages';
 
-import { AppComponent } from './app.component';
-import { MessageComponent } from './shared/message/message.component';
+import {AppComponent} from './app.component';
+import {MessageComponent} from './shared/message/message.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MessageComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    MessagesModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MessageComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpModule,
+        MessagesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- chapter1/primeng-webpack-setup/src/app/app.module.ts
+++ chapter1/primeng-webpack-setup/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 import {APP_BASE_HREF} from '@angular/common';
 
@@ -14,6 +15,7 @@ import {routes} from './app-routing.module';
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         routes,
         FormsModule,
         ButtonModule,
