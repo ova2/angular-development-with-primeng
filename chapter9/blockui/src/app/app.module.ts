01e729330ea73bcf1a0f95fbdccdef982ad50e16
Oleg Varaksin <ovaraksin@googlemail.com>
2017-05-05 00:46:50 +0200

    Updated dependencies

--- chapter9/blockui/src/app/app.module.ts
+++ chapter9/blockui/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 import {APP_BASE_HREF} from '@angular/common';
 
 // import needed PrimeNG modules here
 import {BlockUIModule} from 'primeng/components/blockui/blockui';
-import {RadioButtonModule} from 'primeng/components/radiobutton/radiobutton';
-import {GrowlModule} from 'primeng/components/growl/growl';
 
 import {AppComponent}  from './app.component';
 import {BlockUIComponent}  from './section/blockui.component';
@@ -15,11 +14,10 @@ import {routes} from './app-routing.module';
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         routes,
         FormsModule,
-        BlockUIModule,
-        RadioButtonModule,
-        GrowlModule
+        BlockUIModule
     ],
     declarations: [
         AppComponent,
