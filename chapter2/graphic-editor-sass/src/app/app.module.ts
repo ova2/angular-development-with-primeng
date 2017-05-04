ffea460d497ea84d9e860a3198dab325852e855b
Oleg Varaksin <ovaraksin@googlemail.com>
2017-05-05 00:40:13 +0200

    Updated dependencies

--- chapter2/graphic-editor-sass/src/app/app.module.ts
+++ chapter2/graphic-editor-sass/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 import {APP_BASE_HREF} from '@angular/common';
 
@@ -18,6 +19,7 @@ import {ToolbarComponent} from './panel/toolbar/toolbar.component';
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         routes
     ],
