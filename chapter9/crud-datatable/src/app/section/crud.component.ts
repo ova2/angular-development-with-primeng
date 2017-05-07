0c7f7319cb273c926c762b6f71bddf394f511251
Oleg Varaksin <ovaraksin@googlemail.com>
2017-05-07 23:08:13 +0200

    Unused import

--- chapter9/crud-datatable/src/app/section/crud.component.ts
+++ chapter9/crud-datatable/src/app/section/crud.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, OnDestroy, ChangeDetectionStrategy} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {EmployeeService} from './service/employee.service';
 import {Employee} from '../model/employee';
 import {Message} from 'primeng/components/common/api';
